7b2927229853eb82c88dfd75fc96d862b0f4b845
ramjith246 <ramjithpk2003@gmail.com>
2025-03-02 03:37:16 +0530

    first 2

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ admin.initializeApp({
       token_uri: process.env.FIREBASE_TOKEN_URI,
       auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
       client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-      universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+      
     }),
   });
 
